fbf0cb9dd2d670673eb00b9a2d06fb9df00289ed
Rolan Gonzalez <riolcrt@gmail.com>
2020-10-20 12:32:57 +0200

    [nerdl] add angular material

--- tools/ner-dataset-labeler/src/app/app.module.ts
+++ tools/ner-dataset-labeler/src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TextboxComponent } from './textbox/textbox.component';
+import { AutofocusDirective } from './autofocus.directive';
+import { FormsModule } from '@angular/forms';
+import { TextPreviewComponent } from './text-preview/text-preview.component';
+
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextboxComponent,
+    AutofocusDirective,
+    TextPreviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]

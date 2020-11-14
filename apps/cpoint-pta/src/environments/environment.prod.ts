688f3e3a168e388b8188ad4bd562b2c6d88b7198
Rolan Gonzalez <riolcrt@gmail.com>
2020-11-14 05:57:29 +0100

    [main] the minimum error

--- apps/cpoint-pta/src/environments/environment.prod.ts
+++ apps/cpoint-pta/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   LEX_URL: 'https://cpoint-pta-backend-xvpevfpqeq-ew.a.run.app',
-  GPT3_URL: 'http://cpoint-pta-backend-xvpevfpqeq-ew.a.run.app/gpt3',
-  Knowledge_URL: 'http://cpoint-pta-backend-xvpevfpqeq-ew.a.run.app/knowledge-graph',
+  GPT3_URL: 'https://cpoint-pta-backend-xvpevfpqeq-ew.a.run.app/gpt3',
+  Knowledge_URL: 'https://cpoint-pta-backend-xvpevfpqeq-ew.a.run.app/knowledge-graph',
   GRAPHQL_URL: 'https://cpoint-kga-xvpevfpqeq-ew.a.run.app'
 };
 
